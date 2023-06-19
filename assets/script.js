d451000cd1a945de972efccff3bd8439289aa3ee
Dynashia Lewis <dlewis0985@yahoo.com>
2023-06-19 04:32:07 -0400

    work

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+
+
+
+// set the event listeners
+startQuizBtn.addEventListener("click", startQuiz);
+highScoresBtn.addEventListener("click", highScores);
+submitBtn.addEventListener("click", onSubmitClick);
+answerChoiceContainer.addEventListener("click", answerChoiceClicked)
